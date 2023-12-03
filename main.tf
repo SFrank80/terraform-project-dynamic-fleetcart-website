482c82bd929d1020e8f001a2513160e213496648
Shameeka Franklin <shameeka.franklin@gmail.com>
2023-12-02 19:12:22 -0500

    Deleted and created new files

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 # configure aws provider
 provider "aws" {
   region = "us-east-1"
-  profile = "terraform-userMeek2"
+  profile = "Meeka1-terraform"
 }
 
-# stores the terraform state file in s3
+# Stores the terraform file in s3
 terraform {
   backend "s3" {
-    bucket = "meek2-terraform-remote-state"
+    bucket = "meeka1-terraform-remote-state"
     key    = "terraform.tfstate.dev"
     region = "us-east-1"
-    profile = "terraform-userMeek2"
+    profile = "Meeka1-terraform"
   }
 }
